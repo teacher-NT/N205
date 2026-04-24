68c844771e9071d49beaadd526dc30ad8470c1fb
Teacher NT <holmonbek@gmail.com>
2026-04-24 19:37:02 +0500

    set methods

--- main.py
+++ main.py
@@ -1,41 +1,31 @@
 import os
 os.system("cls")
 
-# set1 = {13,32,14,25}
-# set1.add(11)
-# set1.add(60)
-# set1.add(22)
+set1 = {'olma', 'anor', 'gilos', 'banan', 'uzum'}
+set2 = {'qovun', 'tarvuz', 'uzum', 'banan', 'olma'}
+
+# set3 = set1.intersection(set2)
+# print(set3)
+
+# set1.intersection_update(set2)
 # print(set1)
 
-# set1 = {"olma", 'anor', 'gilos', 'banan'}
-# set1.remove('anor')
+# set3 = set1.difference(set2)
+# set3 = set2.difference(set1)
+# print(set3)
+
+# set1.difference_update(set2)
 # print(set1)
 
-# set1 = {"olma", 'anor', 'gilos', 'banan'}
-# n = set1.pop()
-# print(n)
+# set3 = set1.symmetric_difference(set2)
+# print(set3)
+
+# set1.symmetric_difference_update(set2)
 # print(set1)
 
-# set1 = {"olma", 'anor', 'gilos', 'banan'}
-# set1.discard('apelsin')
-# print(set1)
 
-# set1 = {"olma", 'anor', 'gilos', 'banan'}
-# set1.clear()
-# print(set1)
+set1 = {1,2,3,4,5,6,7,8,9,10}
+set2 = {4,5,6}
 
-# set1 = {"olma", 'anor', 'gilos', 'banan'}
-# # set2 = set1.copy()
-# set2 = set1
-# print(set2)
-
-# set1 = {"olma", 'anor', 'gilos', 'banan'}
-# lst = {'gilos', 'banan', 1,2,3,4}
-# set1.update(lst)
-# print(set1)
-
-# set1 = {"olma", 'anor', 'gilos', 'banan'}
-# lst = {'gilos', 'banan', 1,2,3,4}
-# set2 = set1.union(lst)
-# print(set1)
-# print(set2)
+print(set2.issubset(set1))
+print(set1.issuperset(set2))
