002b8ca12e0648a21699086b6810b95a6a21f747
Teacher NT <holmonbek@gmail.com>
2026-04-24 19:04:37 +0500

    Methods

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 import os
 os.system("cls")
 
-# set1 = {1,2,3,8,7,33,23,98,23,7,2,3}
+# set1 = {13,32,14,25}
+# set1.add(11)
+# set1.add(60)
+# set1.add(22)
+# print(set1)
 
-# for i in set1:
-#     print(i, end=" ")
+# set1 = {"olma", 'anor', 'gilos', 'banan'}
+# set1.remove('anor')
+# print(set1)
 
-set2 = {"Olma", 'Anor', 'GIlos', "O'rik"}
-if "Anor" in set2:
-    print("Yes")
-else:
-    print("No")
+# set1 = {"olma", 'anor', 'gilos', 'banan'}
+# n = set1.pop()
+# print(n)
+# print(set1)
+
+# set1 = {"olma", 'anor', 'gilos', 'banan'}
+# set1.discard('apelsin')
+# print(set1)
+
+# set1 = {"olma", 'anor', 'gilos', 'banan'}
+# set1.clear()
+# print(set1)
+
+# set1 = {"olma", 'anor', 'gilos', 'banan'}
+# # set2 = set1.copy()
+# set2 = set1
+# print(set2)
+
+# set1 = {"olma", 'anor', 'gilos', 'banan'}
+# lst = {'gilos', 'banan', 1,2,3,4}
+# set1.update(lst)
+# print(set1)
+
+# set1 = {"olma", 'anor', 'gilos', 'banan'}
+# lst = {'gilos', 'banan', 1,2,3,4}
+# set2 = set1.union(lst)
+# print(set1)
+# print(set2)
