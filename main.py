60f217176b04062330364245fb731bf11c33ab1b
Teacher NT <holmonbek@gmail.com>
2026-04-24 18:39:55 +0500

    Set

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import os
 os.system("cls")
 
-print("Salom dunyo")
+# set1 = {1,2,3,8,7,33,23,98,23,7,2,3}
+
+# for i in set1:
+#     print(i, end=" ")
+
+set2 = {"Olma", 'Anor', 'GIlos', "O'rik"}
+if "Anor" in set2:
+    print("Yes")
+else:
+    print("No")
