b1b689ea8a7cc9109ae78eed558c5f38fd81f220
Teacher NT <holmonbek@gmail.com>
2026-04-27 18:56:09 +0500

    DIct methods

--- main.py
+++ main.py
@@ -4,14 +4,39 @@ os.system("cls")
 car = {
     "brand": "Chevrolet",
     "model": "Cobalt",
-    "rang": "oq",
+    "rang": ["oq", "qora", "qizil", "ko'k","yashil"],
     "narx": 12000,
     "ish-turi": "Taksi",
     "probeg": 80_000,
-    "rang": "qora"
 }
+# print(car["yil"])
+# print(car.get('yil', "topilmadi"))
 
-car["brand"] = "GM"
-car['yil'] = 2023
-print(car['model'], car["brand"], sep=", ")
+# print(list(car.keys()))
+
+# print(list(car.values()))
+
+# k = car.pop("probeg")
+# print(car)
+# print(k)
+
+# car2 = car.copy()
+# print(car2)
+
+# print(car.items())
+# for i,j in car.items():
+#     print(i, j)
+
+# car.clear()
+# print(car)
+
+# k = car.popitem()
+# print(k)
+# print(car)
+
+# car['narx'] = 9000
+# car['ish-turi'] = 'dastavka'
+# car['probeg'] = 100000
+
+car.update({"narx": 9000, 'ish-turi':'dastavka', 'probeg':100000})
 print(car)
