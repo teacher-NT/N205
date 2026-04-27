e3fe951fd4878ade5269effff2529f52ee7e10f3
Teacher NT <holmonbek@gmail.com>
2026-04-27 18:28:12 +0500

    DUct

--- main.py
+++ main.py
@@ -1,31 +1,17 @@
 import os
 os.system("cls")
 
-set1 = {'olma', 'anor', 'gilos', 'banan', 'uzum'}
-set2 = {'qovun', 'tarvuz', 'uzum', 'banan', 'olma'}
+car = {
+    "brand": "Chevrolet",
+    "model": "Cobalt",
+    "rang": "oq",
+    "narx": 12000,
+    "ish-turi": "Taksi",
+    "probeg": 80_000,
+    "rang": "qora"
+}
 
-# set3 = set1.intersection(set2)
-# print(set3)
-
-# set1.intersection_update(set2)
-# print(set1)
-
-# set3 = set1.difference(set2)
-# set3 = set2.difference(set1)
-# print(set3)
-
-# set1.difference_update(set2)
-# print(set1)
-
-# set3 = set1.symmetric_difference(set2)
-# print(set3)
-
-# set1.symmetric_difference_update(set2)
-# print(set1)
-
-
-set1 = {1,2,3,4,5,6,7,8,9,10}
-set2 = {4,5,6}
-
-print(set2.issubset(set1))
-print(set1.issuperset(set2))
+car["brand"] = "GM"
+car['yil'] = 2023
+print(car['model'], car["brand"], sep=", ")
+print(car)
