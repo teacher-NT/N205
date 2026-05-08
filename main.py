b010b5acbdd30983e19c158b8a28fd4822ba133a
Teacher NT <holmonbek@gmail.com>
2026-05-08 18:47:20 +0500

    Binary file

--- main.py
+++ main.py
@@ -1,37 +1,9 @@
 import os
 os.system("cls")
-import json
 
-# users= [
-#     {
-#         "username": "ozodoov__",
-#         "followers":500,
-#         "following": 100,
-#         "posts_count": 0
-#     },
-#     {
-#         "username": "suhrob",
-#         "followers":500,
-#         "following": 1200,
-#         "posts_count": 6000
-#     },
-#     {
-#         "username": "Sherali",
-#         "followers":220,
-#         "following": 340,
-#         "posts_count": 32
-#     },
-#     {
-#         "username": "Ayubxon",
-#         "followers":5000,
-#         "following": 280,
-#         "posts_count": 40
-#     }
-# ]
+with open("image.jpeg", "rb") as file:
+    baytlar = list(file.read())
+    # for i in baytlar:
+    #     print(i, end=" ")
 
-# file = open("users.json", "w")
-# json.dump(users, file, indent=4)
-
-file = open("users.txt")
-users = json.load(file)
-print(users)
+    print(f"\n{len(baytlar)} ta raqam")
