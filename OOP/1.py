0187bf1252da15398d94f17fe8fb534d4e73360d
Teacher NT <holmonbek@gmail.com>
2026-05-15 18:31:58 +0500

    OOP 1

---
+++ OOP/1.py
@@ -0,0 +1,21 @@
+import os
+os.system("cls")
+
+class Car:
+    brand = "BMW"
+    model = "M8"
+    price = 200000
+    color = 'black'
+
+car1 = Car()
+print(car1.brand)
+print(car1.model)
+
+a = 12
+b = 4.5
+c = 'Salom'
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(car1))
