0c846c236ef98b4ce0176788dbb30b0b2d6ca141
Teacher NT <holmonbek@gmail.com>
2026-05-20 18:58:15 +0500

    polimorfizm

---
+++ OOP/13.py
@@ -0,0 +1,16 @@
+import os
+os.system("cls")
+
+class Dog:
+    def speak(self):
+        print("Dog is barking...")
+
+class Cat:
+    def speak(self):
+        print("Cat is mewing...")
+
+dog1 = Dog()
+cat1 = Cat()
+
+dog1.speak()
+cat1.speak()
