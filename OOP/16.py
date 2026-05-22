e80adc23245f822cd162de6a4548138104163e9f
Teacher NT <holmonbek@gmail.com>
2026-05-22 18:37:55 +0500

    getter va setter

--- OOP/16.py
+++ OOP/16.py
@@ -5,9 +5,24 @@ class BankAccount:
     def __init__(self, n, b):
         self.name = n
         self.__balance = b
+    
+    def get_balance(self, parol):
+        if parol == 'qwerty':
+            print(self.__balance)
+        else:
+            print("Parol xato!")
+    def set_balance(self, new, parol):
+        if parol == 'qwerty':
+            self.__balance = new
+            print("Balans o'zgardi")
+        else:
+            print("Parol xato!")
 
     
 b1 = BankAccount("Suhrob", 1000)
 print(b1.name)
 # b1.__balance = 2000
-print(b1.__balance)
+# print(b1.__balance)
+b1.get_balance("qwerty")
+b1.set_balance(1500, "12ewcwe")
+b1.get_balance("qwerty")
