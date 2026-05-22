7fdc25ea9a6cdc90f3c7337f81bf454067c8819f
Teacher NT <holmonbek@gmail.com>
2026-05-22 18:48:39 +0500

    protected

--- OOP/16.py
+++ OOP/16.py
@@ -2,9 +2,10 @@ import os
 os.system("cls")
 
 class BankAccount:
-    def __init__(self, n, b):
+    def __init__(self, n, b, p):
         self.name = n
         self.__balance = b
+        self._parol = p
     
     def get_balance(self, parol):
         if parol == 'qwerty':
@@ -19,10 +20,12 @@ class BankAccount:
             print("Parol xato!")
 
     
-b1 = BankAccount("Suhrob", 1000)
+b1 = BankAccount("Suhrob", 1000, "qwerty")
 print(b1.name)
 # b1.__balance = 2000
-# print(b1.__balance)
-b1.get_balance("qwerty")
-b1.set_balance(1500, "12ewcwe")
-b1.get_balance("qwerty")
+print(b1.__balance)
+# b1.get_balance("qwerty")
+# b1.set_balance(1500, "12ewcwe")
+# b1.get_balance("qwerty")
+
+print(b1._parol)
