feef4e5a411c7d2b3a2e39f695429caf52b351c5
Teacher NT <holmonbek@gmail.com>
2026-06-15 18:40:33 +0500

    mysql connector

---
+++ SQL4/1.py
@@ -0,0 +1,26 @@
+import os
+os.system("cls")
+
+from mysql import connector
+
+mydb = connector.connect(
+    host = "localhost",
+    user="root",
+    password="1234",
+    database="dars_db"
+)
+
+cursor =  mydb.cursor()
+
+cursor.execute("SELECT * FROM users;")
+
+# jadval = cursor.fetchall()
+# for i in jadval:
+    # print(i)
+
+# jadval = cursor.fetchone()
+# print(jadval)
+
+jadval = cursor.fetchmany(5)
+for i in jadval:
+    print(i)
